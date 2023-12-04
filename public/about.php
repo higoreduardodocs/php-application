1f5ce48ad746e9bbc4516d90cdaa490989a545b4
higoreduardodocs <higoreduardodocs@gmail.com>
2023-12-04 10:57:11 -0400

    :sparkles: Feat: Create about page

---
+++ public/about.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+  <?php include './header.php'; ?>
+
+  <div class="heading">
+    <h3>About us</h3>
+    <p><a href="./index.php">Home</a> / About</p>
+  </div>
+
+  <section class="about">
+    <div class="image">
+      <img src="./images/about-img.jpg" alt="About">
+    </div>
+    <div class="content">
+      <h3>Why choose us?</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptatibus aut hic molestias, reiciendis natus fuga, cumque excepturi veniam ratione iure. Excepturi fugiat placeat iusto facere id officia assumenda temporibus?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quos enim minima ipsa dicta officia corporis ratione saepe sed adipisci?</p>
+      <a href="./contact.php" class="btn purple">Contact us</a>
+    </div>
+  </section>
+
+  <section class="reviews">
+    <h1 class="title">Client's reviews</h1>
+    <div class="box-container">
+      <div class="box">
+        <img src="images/pic-1.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/pic-2.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/pic-3.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/pic-4.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/pic-5.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/pic-6.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+   </div>
+  </section>
+
+  <section class="authors">
+   <h1 class="title">Greate authors</h1>
+   <div class="box-container">
+      <div class="box">
+        <img src="images/author-1.jpg" alt="">
+        <div class="share">
+          <a href="#" class="fab fa-facebook-f"></a>
+          <a href="#" class="fab fa-twitter"></a>
+          <a href="#" class="fab fa-instagram"></a>
+          <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/author-2.jpg" alt="">
+        <div class="share">
+          <a href="#" class="fab fa-facebook-f"></a>
+          <a href="#" class="fab fa-twitter"></a>
+          <a href="#" class="fab fa-instagram"></a>
+          <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/author-3.jpg" alt="">
+        <div class="share">
+          <a href="#" class="fab fa-facebook-f"></a>
+          <a href="#" class="fab fa-twitter"></a>
+          <a href="#" class="fab fa-instagram"></a>
+          <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/author-4.jpg" alt="">
+        <div class="share">
+          <a href="#" class="fab fa-facebook-f"></a>
+          <a href="#" class="fab fa-twitter"></a>
+          <a href="#" class="fab fa-instagram"></a>
+          <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/author-5.jpg" alt="">
+        <div class="share">
+          <a href="#" class="fab fa-facebook-f"></a>
+          <a href="#" class="fab fa-twitter"></a>
+          <a href="#" class="fab fa-instagram"></a>
+          <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+
+      <div class="box">
+        <img src="images/author-6.jpg" alt="">
+        <div class="share">
+          <a href="#" class="fab fa-facebook-f"></a>
+          <a href="#" class="fab fa-twitter"></a>
+          <a href="#" class="fab fa-instagram"></a>
+          <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>John Doe</h3>
+      </div>
+   </div>
+  </section>
+
+  <?php include './footer.php'; ?>
+  <script src="./js/script.js"></script>
+</body>
+</html>
